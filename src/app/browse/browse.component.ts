d2da7b2e36e92504057e18561af6ccba7ba86b30
Ansar Madini Baba <ansar.madinibaba@gmail.com>
2020-03-03 20:57:25 +0530

    Deatils of one customer

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
     selector: "Browse",
-    templateUrl: "./browse.component.html"
+    templateUrl: "./browse.component.html",
+    styleUrls:["./browse.component.scss"],
 })
 export class BrowseComponent implements OnInit {
 
-    constructor() {
+    constructor(private routerExtensions: RouterExtensions) {
         // Use the component constructor to inject providers.
     }
 
@@ -19,5 +21,9 @@ export class BrowseComponent implements OnInit {
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    onNavBtnTap(){
+        this.routerExtensions.back();
     }
 }
